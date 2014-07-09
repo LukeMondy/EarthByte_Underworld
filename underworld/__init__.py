c5da9cca14cefb9dc3316c2492c95c1357291929
JulianGiordani <none@none>
2014-07-09 06:51:27 +0000

    Nothing worked without this suble fix

--- underworld/__init__.py
+++ underworld/__init__.py
@@ -41,7 +41,7 @@ _signal.signal(_signal.SIGINT, _signal_handler)
 
 # lets go right ahead and init now.  user can re-init if necessary.
 import _stgermain
-_stgermain.StgInit()
+Init()
 _rank = _stgermain.getData().rank
 _nprocs = _stgermain.getData().nProcs
 
