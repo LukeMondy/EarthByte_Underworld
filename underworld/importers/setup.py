9baaebb62d57bf4773b2423dfcda711e0349c18b
bmather <none@none>
2014-08-19 21:17:36 +1000

    added tools to import Gocad voxels in python

---
+++ underworld/importers/setup.py
@@ -0,0 +1,5 @@
+# Importers module - replaces various module XML files in the Underworld Importers Toolbox
+
+# Setup simply imports all the definitions files which are bulky strings of json dictionary stuff
+
+from _importers_setup import *
