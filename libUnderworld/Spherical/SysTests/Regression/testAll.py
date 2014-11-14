4040a1a59b271ce26eb426989e14eada65de30e3
Julian Giordani <none@none>
2014-11-14 14:12:03 +0000

    Adding a test for 2D annulus with periodic bcs

--- libUnderworld/Spherical/SysTests/Regression/testAll.py
+++ libUnderworld/Spherical/SysTests/Regression/testAll.py
@@ -5,12 +5,15 @@ from credo.systest import *
 testSuite = SysTestSuite("Spherical", "RegressionTests")
 
 models = ["LidDrivenFEM.xml",
-       "LidDrivenPIC.xml" ]
+          "LidDrivenPIC.xml",
+          "Periodic.annulus.xml" ]
 
 # testing spherical-segment mesh
 for np in [1,2]:
   testSuite.addStdTest(ReferenceTest, "LidDrivenFEM.xml", runSteps=0, nproc=np)
-  testSuite.addStdTest(ReferenceTest, "LidDrivenPIC.xml", runSteps=5, nproc=np)
+
+testSuite.addStdTest(ReferenceTest, "LidDrivenPIC.xml", runSteps=5, nproc=1)
+testSuite.addStdTest(ReferenceTest, "Periodic.annulus.xml", runSteps=1, nproc=1)
 
 # Set to a high time, as extension test can be quite slow on older procs
 testSuite.setAllTimeouts(minutes=60)
