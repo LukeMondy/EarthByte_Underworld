9ebc6a7216e542dbbd0c86a85188de87e68aad56
dave lee <davelee2804@gmail.com>
2015-01-21 15:25:00 +1100

    allowing for restarts for the regional sphere mesh generator

--- libUnderworld/Spherical/Components/src/RSGenerator.c
+++ libUnderworld/Spherical/Components/src/RSGenerator.c
@@ -88,10 +88,55 @@ RSGenerator* _RSGenerator_New(  SPHERICALGENERATOR_DEFARGS  )
 
 void _RSGenerator_AssignFromXML( void* meshGenerator, Stg_ComponentFactory* cf, void* data )
 {
-   RSGenerator*		self = (RSGenerator*)meshGenerator;
+   RSGenerator*			self 		= (RSGenerator*)meshGenerator;
+   FiniteElementContext*	context 	= Stg_ComponentFactory_ConstructByName( cf, (Name)"context", FiniteElementContext, True, NULL  );
+   Dictionary*			dict		= Dictionary_Entry_Value_AsDictionary( Dictionary_Get( cf->componentDict, (Dictionary_Entry_Key)self->name ) );
+   Dictionary_Entry_Value	*minList, *maxList, *tmp;
+   char*			rootKey;
+   unsigned			d_i;
+   double 			maxVal;
 
    _CartesianGenerator_AssignFromXML( meshGenerator, cf, data );
 
+   // mesh min/max coords are written to file from the mesh->min/maxGlobalCoord field. these differ from the min/max
+   // coords as defined in the RSGenerator->min/maxCrd (these are in r-theta-phi, not x-y-z), so if we're restarting
+   // we need to over-ride the coords from the hdf5 mesh file with those from the xml (assuming they don't differ
+   // between runs...
+   if( self->nDims > 2 ) {
+      if( context->loadFromCheckPoint ) {
+         minList = Dictionary_Get( dict, (Dictionary_Entry_Key)"minCoord" );
+         maxList = Dictionary_Get( dict, (Dictionary_Entry_Key)"maxCoord" );
+         if( minList && maxList ) {
+                assert( Dictionary_Entry_Value_GetCount( minList ) >= self->nDims );
+                assert( Dictionary_Entry_Value_GetCount( maxList ) >= self->nDims  );
+                for( d_i = 0; d_i < self->nDims; d_i++ ) {
+                   tmp = Dictionary_Entry_Value_GetElement( minList, d_i );
+                   rootKey = Dictionary_Entry_Value_AsString( tmp );
+
+                   if( !Stg_StringIsNumeric( (char*)rootKey ) )
+                      tmp = Dictionary_Get( cf->rootDict, (Dictionary_Entry_Key)rootKey );
+                   self->crdMin[d_i] = Dictionary_Entry_Value_AsDouble( tmp );
+
+                   tmp = Dictionary_Entry_Value_GetElement( maxList, d_i );
+                   rootKey = Dictionary_Entry_Value_AsString( tmp );
+
+                   if( !Stg_StringIsNumeric( (char*)rootKey ) )
+                      tmp = Dictionary_Get( cf->rootDict, (Dictionary_Entry_Key)rootKey );
+                   self->crdMax[d_i] = Dictionary_Entry_Value_AsDouble( tmp );
+                   /* test to ensure provided domain is valid */
+                   maxVal = (abs(self->crdMax[d_i]) > abs(self->crdMin[d_i])) ? abs(self->crdMax[d_i]) : abs(self->crdMin[d_i]);
+                   if( maxVal == 0 ) maxVal = 1;  /* if maxVal is zero, then both numbers must be zero, set to one as next test will fail */
+                   Journal_Firewall( ( ( (self->crdMax[d_i] - self->crdMin[d_i])/maxVal) > 1E-10 || d_i==J_AXIS), global_error_stream,
+                     "\n\nError in %s for %s '%s'\n\n"
+                     "Dimension of domain (min = %f, max = %f) for component number %u is not valid.\n\n",
+                     __func__, self->type, self->name, self->crdMin[d_i], self->crdMax[d_i], d_i );
+                }
+         }
+      }
+      // phi = (pi/2, pi)
+      Journal_Firewall( !(fabs(self->crdMin[2] -self->crdMax[2])>=179.99 || self->crdMax[2] < self->crdMin[2]),
+         global_error_stream, "\nError in %s: Phi definition is wrong. Ensure minZ < maxZ & abs(minZ-maxZ)<180\n", __func__);
+   }
    Journal_Firewall( self->nDims==3, global_error_stream,
         "Error in %s: Must have 3 dimensions for component %s\n", __func__, self->name );
    // check domain size is valid
@@ -99,14 +144,7 @@ void _RSGenerator_AssignFromXML( void* meshGenerator, Stg_ComponentFactory* cf, 
         "Error in %s: Radius definition is wrong. Ensure maxX > minX & minX > 0\n", __func__ );
 
    Journal_Firewall(!( fabs(self->crdMin[1]-self->crdMax[1]) >= 179.99 || self->crdMax[1] < self->crdMin[1] ),
-         global_error_stream,
-         "Error in %s: Theta definition is wrong. Ensure minY < maxY && abs(minY-maxY) < 180\n", __func__ );
-
-   if( self->nDims > 2 ) {
-      // phi = (pi/2, pi)
-      Journal_Firewall( !(fabs(self->crdMin[2] -self->crdMax[2])>=179.99 || self->crdMax[2] < self->crdMin[2]),
-         global_error_stream, "\nError in %s: Phi definition is wrong. Ensure minZ < maxZ & abs(minZ-maxZ)<180\n", __func__);
-   }
+         global_error_stream, "Error in %s: Theta definition is wrong. Ensure minY < maxY && abs(minY-maxY) < 180\n", __func__ );
 }
 
 void _RSGenerator_Initialise( void* meshGenerator, void* data )
